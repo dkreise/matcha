93c5539c35f0de1b6f556adb1b109d5260186cec
Diana Kreise <dia.kreis@gmail.com>
2025-04-29 13:07:36 +0200

    added initial tags to db

--- srcs/backend/db/migrations/002_create_tags.js
+++ srcs/backend/db/migrations/002_create_tags.js
@@ -5,4 +5,38 @@ module.exports.up = async (pool) => {
             name VARCHAR(100) UNIQUE NOT NULL
         );
     `);
+
+    await pool.query(`
+        INSERT INTO tags (name)
+        VALUES 
+          ('vegan'),
+          ('geek'),
+          ('music'),
+          ('travel'),
+          ('sport'),
+          ('piercing'),
+          ('tattoos'),
+          ('introvert'),
+          ('extrovert'),
+          ('gamer'),
+          ('reader'),
+          ('coffee'),
+          ('tea'),
+          ('coding'),
+          ('matcha'),
+          ('photography'),
+          ('art'),
+          ('nature'),
+          ('fashion'),
+          ('pets'),
+          ('movies'),
+          ('hiking'),
+          ('yoga'),
+          ('fitness'),
+          ('cooking'),
+          ('dancing'),
+          ('writing'),
+          ('swimming')
+        ON CONFLICT (name) DO NOTHING;
+    `);
 };
