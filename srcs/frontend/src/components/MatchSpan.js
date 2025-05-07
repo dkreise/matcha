69b2ca90fcd214a8556364f7d89b9415bb349369
Diana Kreise <dia.kreis@gmail.com>
2025-05-07 15:53:04 +0200

    created match component

---
+++ srcs/frontend/src/components/MatchSpan.js
@@ -0,0 +1,9 @@
+const MatchSpan = ({ text }) => {
+    return (
+        <span className="bg-like-light text-sm rounded-md px-2 py-1 text-like-dark font-bold">
+            {text}
+        </span>
+    );
+};
+
+export default MatchSpan;
